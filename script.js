d714bcf7d3031bca5a53c340df750bde007e4607
daramsreya <daramsreya83@gmail.com>
2025-07-23 21:35:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //your JS code here. If required.
-const elementParent=document.getEelmentById("level");
+const elementParent=document.getElementById("level");
 function getLevel(element){
 	let level=0;
 	while(element){
-		element=element.parentElement();
+		element=element.parentElement;
 		level++;
 	}
 	return level;
 }
 const result=getLevel(elementParent);
-alert(`The level of the element is: $(result)`);
+alert(`The level of the element is: ${result}`);
